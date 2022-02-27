d96a103baf49e9e27902eadc9170ad91663f35d6
NomadCoder101 <cbinary07@gmail.com>
2022-02-27 13:04:35 -0500

    Shopping Site v1

---
+++ store/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from category.models import Category
+from django.urls import reverse
+
+# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255, unique=True)
+    slug= models.SlugField(max_length=255, unique=True)
+    description=models.TextField(max_length=500, blank=True)
+    image=models.ImageField(upload_to='photos/products',blank=True)
+    stock=models.IntegerField()
+    price=models.IntegerField()
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_available =models.BooleanField(default=True)
+    created_date= models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse(
+            'product_detail',args=[self.category.slug, self.slug]
+        )
+
+    def __str__(self):
+        return self.product_name
+
+
+
+class Variation_Manager(models.Manager):
+
+    def colors(self):
+        return super(Variation_Manager,self,).filter(variation_category = 'color', is_active=True)
+    
+    def sizes(self):
+        return super(Variation_Manager,self,).filter(variation_category = 'size', is_active=True)
+
+
+variation_category_choice =(
+    ('color', 'color'),
+    ('size','size'),
+
+ )
+
+class Variation(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date= models.DateTimeField(auto_now_add=True)
+
+    objects = Variation_Manager()
+
+    def __str__(self):
+        return self.variation_value
+
+
+
+    
+
+
+
+
